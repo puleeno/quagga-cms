31962da940b0723055b9d87e07b52c30c11d4bb5
Puleeno <puleeno@gmail.com>
2024-03-18 00:01:19 +0700

    create auth config

---
+++ configs/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+  'defaults' => [
+    'guard' => 'web',
+    'passwords' => 'users',
+  ],
+  'providers' => [
+    'users' => [
+        'model' => 'App\Models\User',
+    ],
+  ],
+  'controllers' => [
+    'login' => 'App\Http\Controllers\Auth\LoginController',
+    'register' => 'App\Http\Controllers\Auth\RegisterController',
+    'reset' => 'App\Http\Controllers\Auth\ResetPasswordController',
+  ],
+  'password_timeout' => 10800,
+];
