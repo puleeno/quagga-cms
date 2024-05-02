c6bbb894607f362682cfebd05319b1e9567310db
Puleeno Nguyen <puleeno@gmail.com>
2024-05-02 15:05:22 +0700

    create app config

---
+++ config/app.php
@@ -0,0 +1,20 @@
+<?php
+use App\Providers\RouteServiceProvider;
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Application Environment
+    |--------------------------------------------------------------------------
+    |
+    | This value determines the "environment" your application is currently
+    | running in. This may determine how you prefer to configure various
+    | services the application utilizes. Set this in your ".env" file.
+    |
+    */
+
+    'env' => env('APP_ENV', 'production'),
+
+    'providers' => [
+    ]
+];
